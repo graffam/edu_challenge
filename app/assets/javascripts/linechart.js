3f5f8272420c674dd53aca5229e7ec9ae0707b60
graffam <graffam87@gmail.com>
2015-03-16 22:24:03 -0400

    removes junk in vendor file

--- app/assets/javascripts/linechart.js
+++ app/assets/javascripts/linechart.js
@@ -1,5 +1,6 @@
 $(function() {
   context = document.getElementById('lineChart').getContext('2d');
+  context2 = document.getElementById('lineChart2').getContext('2d');
   var data = {
     labels : ["January","February","March","April","May","June","July"],
     datasets : [
@@ -8,7 +9,7 @@ $(function() {
             strokeColor : "rgba(220,220,220,1)",
             pointColor : "rgba(220,220,220,1)",
             pointStrokeColor : "#fff",
-            data : [65,59,90,81,56,55,40]
+            data : [23,59,90,81,51,55,40]
         },
         {
             fillColor : "rgba(151,187,205,0.5)",
@@ -19,5 +20,25 @@ $(function() {
         }
     ]
 };
+var data2 = {
+  labels : ["January","February","March","April","May","June","July"],
+  datasets : [
+      {
+          fillColor : "rgba(220,220,220,0.5)",
+          strokeColor : "rgba(220,220,220,1)",
+          pointColor : "rgba(220,220,220,1)",
+          pointStrokeColor : "#fff",
+          data : [1,2,3,5,1,5,1]
+      },
+      {
+          fillColor : "rgba(151,187,205,0.5)",
+          strokeColor : "rgba(151,187,205,1)",
+          pointColor : "rgba(151,187,205,1)",
+          pointStrokeColor : "#fff",
+          data : [2,5,1,1,9,8,1]
+      }
+  ]
+};
 new Chart(context).Line(data);
+new Chart(context2).Line(data2);
 });
